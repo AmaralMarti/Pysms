8c62ab43086c43bb184037a2e74548f0a4692d18
tocvieira <thiago@thiagovieira.adv.br>
2016-07-20 09:54:04 -0300

    Sistema funcionando
    Regressamos a urllib.request (padrão da API) e eliminamos os erros
    (campos errados em values).

--- readexcel.py
+++ readexcel.py
@@ -7,7 +7,7 @@ import re
 import urllib.request
 import xlrd
 import configparser
-import requests
+from urllib.request import Request, urlopen
 
 config = configparser.ConfigParser()
 config.read('config.txt')
@@ -26,17 +26,14 @@ while worksheet.cell(n,0).value != xlrd.empty_cell.value: # You can detect an em
     celularfloat = str(celular)
     celular = re.search(r'([0-9]*)', celularfloat)
     celularbr = '55' + celular.group(0)
-    msg = ('Senhora ' + nome.group(0) + 'a CLIMAE confirma sua consulta com o(a) médico(a) ' + worksheet.cell(n,3).value + ' no dia ' + worksheet.cell(n,4).value + '. Responda gratis S para confirmar ou N para cancelar.' + ';' + 'CLIMAE')
-    print (celular.group(0) + ';' + 'Senhor(a) ' + nome.group(0) + 'a CLIMAE confirma sua consulta com o(a) médico(a) ' + worksheet.cell(n,3).value + ' no dia ' + worksheet.cell(n,4).value + '. Responda gratis S para confirmar ou N para cancelar. CLIMAE')
+    msg = ('Sr(a) ' + nome.group(0) + 'confirmamos sua consulta com o(a) médico(a) ' + worksheet.cell(n,3).value + ' no dia ' + worksheet.cell(n,4).value + ' Responda gratis S para confirmar ou N para cancelar.')
+    print (msg)
     values = """
     {
         "sendSmsRequest": {
         "from" : "CLIMAE",
         "to":  "%s",
-        "schedule": "NONE",
         "msg": "%s",
-        "callBackOption": "NONE",
-        "id": "002",
         "aggregateId": "1111"
         }
     }""" % (celularbr, msg)
@@ -46,15 +43,9 @@ while worksheet.cell(n,0).value != xlrd.empty_cell.value: # You can detect an em
         "Accept": "application/json"
     }
     values = values.encode('utf-8')
-#    request = urllib.request.Request("https://api-rest.zenvia360.com.br/services/send-sms", data=values, headers=headers)
-#    response_body = urllib.request.urlopen(request).read()
-    session_requests = requests.session()
-
-    result = session_requests.post(
-        url,
-        data=values,
-        headers=dict(referer=url)
-    )
+    request = urllib.request.Request("https://api-rest.zenvia360.com.br/services/send-sms", data=values, headers=headers)
+    response_body = urllib.request.urlopen(request).read()
+    print (response_body)
     n = n + 1
     if n >= worksheet.nrows:
         break
